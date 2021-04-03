ceee7b6c32c2cb996c8422e7a6a4aaf7d06fe7e7
neripark <neripark@gmail.com>
2021-04-03 11:54:25 +0900

    add sample line

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Layout from '../components/Layout'
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
+    <h2>neripark's playground</h2>
     <p>
       <Link href="/about">
         <a>About</a>
